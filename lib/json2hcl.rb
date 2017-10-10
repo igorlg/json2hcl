d39de50e8a1712f9aa18cf9d9da5b5de8b46d5ad
Igor Gentil <igor.lg@gmail.com>
2017-10-11 11:23:22 +1300

    Add tests for .os and .arch

--- lib/json2hcl.rb
+++ lib/json2hcl.rb
@@ -1,12 +1,14 @@
 require 'json2hcl/version'
 
-module Json2hcl
-  def self.json2hcl
-    bin = "json2hcl_v#{VERSION}_#{os}_#{arch}"
-    File.expand_path(File.join('json2hcl', bin), __dir__)
-  end
+require 'os'
 
-  private
+module Json2hcl
+  # def json2hcl
+  #   bin = "json2hcl_v#{VERSION}_#{os}_#{arch}"
+  #   File.expand_path(File.join('json2hcl', bin), __dir__)
+  # end
+
+  # private
 
   def os
     return 'linux'    if OS.linux?
--- spec/json2hcl_spec.rb
+++ spec/json2hcl_spec.rb
@@ -1,5 +1,120 @@
+require 'os'
+require 'json2hcl'
+
 describe Json2hcl do
-  it 'does something useful' do
-    expect(false).to eq(false)
+  context 'OS = mac, Arch = 64bits' do
+    before do
+      allow(::OS).to receive(:mac?).and_return(true)
+      allow(::OS).to receive(:linux?).and_return(false)
+      allow(::OS).to receive(:windows?).and_return(false)
+      allow(::OS).to receive(:bits).and_return(64)
+    end
+
+    let(:subject)   { Class.new { extend ::Json2hcl } }
+    let(:gem_root)  { File.expand_path('..', __dir__) }
+    let(:version)   { Json2hcl::VERSION }
+
+    it '.os should return "darwin"' do
+      expect(subject.os).to eq 'darwin'
+    end
+
+    it '.arch should return "amd64"' do
+      expect(subject.arch).to eq 'amd64'
+    end
+  end
+
+  context 'OS = mac, Arch = 32bits' do
+    before do
+      allow(::OS).to receive(:mac?).and_return(true)
+      allow(::OS).to receive(:linux?).and_return(false)
+      allow(::OS).to receive(:windows?).and_return(false)
+      allow(::OS).to receive(:bits).and_return(32)
+    end
+
+    let(:subject) { Class.new { extend ::Json2hcl } }
+
+    it '.os should return "darwin"' do
+      expect(subject.os).to eq 'darwin'
+    end
+
+    it '.arch should return "amd64"' do
+      expect(subject.arch).to eq '386'
+    end
+  end
+
+  context 'OS = linux, Arch = 64bits' do
+    before do
+      allow(::OS).to receive(:mac?).and_return(false)
+      allow(::OS).to receive(:linux?).and_return(true)
+      allow(::OS).to receive(:windows?).and_return(false)
+      allow(::OS).to receive(:bits).and_return(64)
+    end
+
+    let(:subject) { Class.new { extend ::Json2hcl } }
+
+    it '.os should return "linux"' do
+      expect(subject.os).to eq 'linux'
+    end
+
+    it '.arch should return "amd64"' do
+      expect(subject.arch).to eq 'amd64'
+    end
+  end
+
+  context 'OS = linux, Arch = 32bits' do
+    before do
+      allow(::OS).to receive(:mac?).and_return(false)
+      allow(::OS).to receive(:linux?).and_return(true)
+      allow(::OS).to receive(:windows?).and_return(false)
+      allow(::OS).to receive(:bits).and_return(32)
+    end
+
+    let(:subject) { Class.new { extend ::Json2hcl } }
+
+    it '.os should return "linux"' do
+      expect(subject.os).to eq 'linux'
+    end
+
+    it '.arch should return "386"' do
+      expect(subject.arch).to eq '386'
+    end
+  end
+
+  context 'OS = windows, Arch = 64bits' do
+    before do
+      allow(::OS).to receive(:mac?).and_return(false)
+      allow(::OS).to receive(:linux?).and_return(false)
+      allow(::OS).to receive(:windows?).and_return(true)
+      allow(::OS).to receive(:bits).and_return(64)
+    end
+
+    let(:subject) { Class.new { extend ::Json2hcl } }
+
+    it '.os should return "windows"' do
+      expect(subject.os).to eq 'windows'
+    end
+
+    it '.arch should return "amd64"' do
+      expect(subject.arch).to eq 'amd64'
+    end
+  end
+
+  context 'OS = windows, Arch = 32bits' do
+    before do
+      allow(::OS).to receive(:mac?).and_return(false)
+      allow(::OS).to receive(:linux?).and_return(false)
+      allow(::OS).to receive(:windows?).and_return(true)
+      allow(::OS).to receive(:bits).and_return(32)
+    end
+
+    let(:subject) { Class.new { extend ::Json2hcl } }
+
+    it '.os should return "windows"' do
+      expect(subject.os).to eq 'windows'
+    end
+
+    it '.arch should return "386"' do
+      expect(subject.arch).to eq '386'
+    end
   end
 end

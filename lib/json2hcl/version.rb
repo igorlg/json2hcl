d72d877c619270388c10993adb128c9c1a97cc0e
Igor Gentil <igor.lg@gmail.com>
2017-10-11 12:18:54 +1300

    Fix version. Add version tests

--- lib/json2hcl/version.rb
+++ lib/json2hcl/version.rb
@@ -1,4 +1,3 @@
 module Json2hcl
-  VERSION = File.read(File.expand_path(File.join('..', '..', 'VERSION'), __dir__)).sub(/v/, '')
-  # VERSION = File.read(File.join(File.expand_path(__dir__), '..', '..', 'VERSION')).sub(/v/, '')
+  VERSION = File.read(File.expand_path(File.join('..', '..', 'VERSION'), __dir__)).sub(/v/, '').strip
 end
--- spec/json2hcl/version_spec.rb
+++ spec/json2hcl/version_spec.rb
@@ -4,7 +4,10 @@ describe 'VERSION' do
   end
 
   it 'returns the version from VERSION file' do
-    version_form_file = File.read(File.expand_path(File.join('..', '..', 'VERSION'), __dir__)).gsub(/v/, '')
+    version_form_file = File.read(File.expand_path(File.join('..', '..', 'VERSION'), __dir__)).gsub(/v/, '').strip
     expect(Json2hcl::VERSION).to match(/#{version_form_file}/)
   end
+
+  it 'doesnt start with "v"'
+  it 'doesnt end in newline'
 end

74db87537aa34669fab07ae1223a33d715001b99
Igor Gentil <igor.lg@gmail.com>
2017-10-11 12:38:56 +1300

    Add version tests

--- spec/json2hcl/version_spec.rb
+++ spec/json2hcl/version_spec.rb
@@ -1,13 +1,32 @@
 describe 'VERSION' do
+  let(:v) { Json2hcl::VERSION }
+
   it 'has a version number' do
-    expect(Json2hcl::VERSION).not_to be nil
+    expect(v).not_to be nil
   end
 
   it 'returns the version from VERSION file' do
     version_form_file = File.read(File.expand_path(File.join('..', '..', 'VERSION'), __dir__)).gsub(/v/, '').strip
-    expect(Json2hcl::VERSION).to match(/#{version_form_file}/)
+    expect(v).to match(/#{version_form_file}/)
   end
 
-  it 'doesnt start with "v"'
-  it 'doesnt end in newline'
+  it 'doesnt start with "v"' do
+    regex = /^v/
+    expect(v).to_not match(regex)
+    expect('v0.1.0').to match(regex) # Just double-checking the regex
+  end
+
+  it 'doesnt end in newline' do
+    regex = /\n/
+    nlstr = "
+    "
+    expect(v).to_not match(regex)
+    expect(nlstr).to match(regex) # Just double-checking the regex
+  end
+
+  it 'is semantic versioned' do
+    regex = /^\d+\.\d+\.\d+$/
+    expect(v).to match(regex)
+    expect('1.0').to_not match(regex)
+  end
 end

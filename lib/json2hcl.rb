cf455104c27e747bab744d9692dbb92fdcff0a66
Igor Gentil <igor.lg@gmail.com>
2017-10-11 13:14:37 +1300

    .

--- lib/json2hcl.rb
+++ lib/json2hcl.rb
@@ -1,9 +1,12 @@
 require 'json2hcl/version'
+require 'json2hcl/os'
 
 module Json2hcl
+  extend ::Json2hcl::OS
+
   def json2hcl
     file = "json2hcl_v#{VERSION}_#{os}_#{arch}"
-    file += '.exe' if OS.os == 'windows'
+    file += '.exe' if os == 'windows'
     File.expand_path(File.join('json2hcl', 'bin', file), __dir__)
   end
 end
--- spec/json2hcl_spec.rb
+++ spec/json2hcl_spec.rb
@@ -1,1 +1,20 @@
 require 'json2hcl'
+require 'json2hcl/os'
+
+describe Json2hcl do
+  before do
+    oshelper  = Class.new { extend ::Json2hcl::OS }
+    @path     = File.expand_path(File.join('..', 'lib', 'json2hcl', 'bin'), __dir__)
+    @os       = oshelper.os
+    @arch     = oshelper.arch
+    @binfile  = bin_file(::Json2hcl::VERSION, '', '', @path)
+  end
+
+  let(:subject) { Class.new { extend ::Json2hcl } }
+
+  describe '.json2hcl' do
+    it 'should return the full path to the json2hcl bin file' do
+      expect(subject.json2hcl).to eq @binfile
+    end
+  end
+end
